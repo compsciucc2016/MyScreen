827600820b981cdf140f723cc49243b559ecd0df
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2016-10-24 19:49:50 -0400

    nick branch :)

--- Mycanvas.java
+++ Mycanvas.java
@@ -7,43 +7,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
-
-/*
-import java.awt.Canvas;
-import java.awt.Graphics;
-import java.awt.Toolkit;
-import java.awt.Color;
-import java.awt.Image;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener; */
-
 public class Mycanvas extends Canvas implements KeyListener, MouseListener {
-	Image img = Toolkit.getDefaultToolkit().getImage("Shrimad.jpeg");
-	int shrimx = 10;
-	int shrimy = 10;
+	Image img = Toolkit.getDefaultToolkit().getImage("snake.png");
+	int posx = 10;
+	int posy = 10;
 	int speed = 10;
 	boolean soundeffect = true;
-	Rectangle rect = new Rectangle(200,300,100,100);
 
 	public Mycanvas() {
 		this.setSize(1200,800);
 		this.addKeyListener(this);
 		this.addMouseListener(this);
 		//this.setFocusable(true);
-		playIt("PPAP.wav");
+		playIt("Elevator-music.wav");
 	}
 
 	public void paint(Graphics g) {
 		//g.setColor(Color.RED);
 		//g.fillOval(99,100,399,99);
-	
-		g.drawImage(img,shrimx,shrimy,200,150,this);
+
+		g.drawImage(img,posx,posy,200,150,this);
 		g.fillRect((int)rect.getX(),(int)rect.getY(),(int)rect.getWidth(),(int)rect.getHeight());
 
 	}
-	
+
 	public void mouseClicked(MouseEvent e) {
 		//System.out.println("Click");
 	}
@@ -51,32 +38,32 @@ public class Mycanvas extends Canvas implements KeyListener, MouseListener {
 		//System.out.println(e);
 	}
 	public void mouseExited(MouseEvent e) {
-		
+
 	}
 	public void mousePressed(MouseEvent e) {
 
-		if (shrimx > this.getWidth() - 202) {
-			shrimx = shrimx - speed;
+		if (posx > this.getWidth() - 202) {
+			posx = posx - speed;
 		}
 
-		if (shrimy > this.getHeight() - 161) {
-			shrimy = shrimy - speed;
+		if (posy > this.getHeight() - 161) {
+			posy = posy - speed;
 		}
 
-		if (shrimx < 0) {
-			shrimx = shrimx + speed;
+		if (posx < 0) {
+			posx = posx + speed;
 		}
 
-		if (shrimy < 0) {
-			shrimy = shrimy + speed;
+		if (posy < 0) {
+			posy = posy + speed;
 		}
 
-		if (rect.contains(shrimx,shrimy)) {
-			shrimx = 10;
-			shrimy = 10;
+		if (rect.contains(posx,posy)) {
+			posx = 10;
+			posy = 10;
 		} else {
-			shrimx = e.getX();
-			shrimy = e.getY();
+			posx = e.getX();
+			posy = e.getY();
 		}
 		this.repaint();
 	}
@@ -86,27 +73,27 @@ public class Mycanvas extends Canvas implements KeyListener, MouseListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == 39) {
-			shrimx = shrimx + speed;
+			posx = posx + speed;
 			//playIt("verynice.wav");
 			this.repaint();
-		} 
+		}
 		if (e.getKeyCode() == 37) {
-			shrimx = shrimx - speed;
+			posx = posx - speed;
 			this.repaint();
 			//playIt("verynice.wav");
 		}
 		if (e.getKeyCode() == 38) {
-			shrimy = shrimy - speed;
+			posy = posy - speed;
 			this.repaint();
 			//playIt("verynice.wav");
 		}
 		if (e.getKeyCode() == 40) {
-			shrimy = shrimy + speed;
+			posy = posy + speed;
 			this.repaint();
 			//playIt("verynice.wav");
 		}
 		if (e.getKeyCode() == 32) {
-			img = Toolkit.getDefaultToolkit().getImage("Shrimdab.jpeg");
+			img = Toolkit.getDefaultToolkit().getImage("fish.png");
 			this.repaint();
 			if(soundeffect == true) {
 				playIt("verynice.wav");
@@ -116,21 +103,21 @@ public class Mycanvas extends Canvas implements KeyListener, MouseListener {
 		if (e.getKeyCode() == 27) {
 			System.exit(0);
 		}
-		if (shrimx > this.getWidth() - 202) {
-			shrimx = shrimx - speed;
+		if (posx > this.getWidth() - 202) {
+			posx = posx - speed;
 		}
-		if (shrimy > this.getHeight() - 161) {
-			shrimy = shrimy - speed;
+		if (posy > this.getHeight() - 161) {
+			posy = posy - speed;
 		}
-		if (shrimx < 0) {
-			shrimx = shrimx + speed;
+		if (posx < 0) {
+			posx = posx + speed;
 		}
-		if (shrimy < 0) {
-			shrimy = shrimy + speed;
+		if (posy < 0) {
+			posy = posy + speed;
 		}
-		if (rect.contains(shrimx,shrimy)) {
-			shrimx = 10;
-			shrimy = 10;
+		if (rect.contains(posx,posy)) {
+			posx = 10;
+			posy = 10;
 		}
 		this.repaint();
 
@@ -139,7 +126,7 @@ public class Mycanvas extends Canvas implements KeyListener, MouseListener {
 	public void keyReleased(KeyEvent e) {
 		//System.out.println(e);
 		if (e.getKeyCode() == 32) {
-			img = Toolkit.getDefaultToolkit().getImage("Shrimad.jpeg");
+			img = Toolkit.getDefaultToolkit().getImage("snake.jpeg");
 			this.repaint();
 			soundeffect = true;
 		}
